3940071a767024a618c2175fc32f3b713188f449
Bruno <Bruno@192.168.101.19>
2016-04-27 21:21:13 -0300

    Merge origin/master
    Conflicts:
    	index.php
    NovoComit

--- cadastrar.php
+++ cadastrar.php
@@ -5,9 +5,11 @@
     </head>
     <body>
         <form method="post" action="salvar.php?acao=1">
-            Nome do Pinguancho: <input type="text" name="nome" required><br>
+            Nome: <input type="text" name="nome" required><br>
             Senha: <input type="password" name="senha" required><br>
             E-Mail: <input type="email" name="email" required><br>
+            Data Nascimento: <input type="date" name="nascimento" required><br>
+            Tipo Usuário: <input type="text" name="tipo" required><br>
             <input type="submit" value="Enviar"/><br>
         </form>
         
