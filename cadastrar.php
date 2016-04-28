f363a3c6a9f0916d8a641842351d9002927fc0de
Bruno <Bruno@192.168.101.19>
2016-04-27 21:45:30 -0300

    Alterações no formulário de cadastro.

--- cadastrar.php
+++ cadastrar.php
@@ -9,20 +9,20 @@
             <div class="form-group">
                 <label for="note3" class="col-sm-2 control-label">Nome</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="note3" placeholder="Nome">
+                    <input type="text" class="form-control" id="note3" placeholder="Nome" required>
                 </div>
             </div>
 
             <div class="form-group">
                 <label for="inputEmail3" class="col-sm-2 control-label">E-Mail</label>
                 <div class="col-sm-10">
-                    <input type="email" class="form-control" id="inputEmail3" placeholder="example@example.com">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="example@example.com" required>
                 </div>
             </div>
             <div class="form-group">
                 <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
                 <div class="col-sm-10">
-                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password" required>
                 </div>
             </div>
             <div class="form-group">
@@ -31,14 +31,14 @@
                         <label>
                             Tipo de Usuário:<br>
                             <input type="radio" name="Tipo" Value="1"> Administrador
-                            <input type="radio" name="Tipo" Value="2"> Autor
+                            <input type="radio" name="Tipo" Value="2" checked="checked"> Autor
                         </label>
                     </div>
                 </div>
             </div>
             <br>
             <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10">
+                <div class="col-sm-offset-2 col-sm-10" required>
                     <button type="submit" class="btn btn-default">Cadastrar</button>
                 </div>
             </div>
