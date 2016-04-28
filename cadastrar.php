8d7f5033898e33855fcd656c9be97c5b4f4a7685
Bruno <Bruno@192.168.101.19>
2016-04-27 21:40:25 -0300

    Merge origin/master
    Conflicts:
    	index.php
    NovoComit

--- cadastrar.php
+++ cadastrar.php
@@ -4,14 +4,44 @@
         <title></title>
     </head>
     <body>
-        <form method="post" action="salvar.php?acao=1">
-            Nome: <input type="text" name="nome" required><br>
-            Senha: <input type="password" name="senha" required><br>
-            E-Mail: <input type="email" name="email" required><br>
-            Data Nascimento: <input type="date" name="nascimento" required><br>
-            Tipo Usuário: <input type="text" name="tipo" required><br>
-            <input type="submit" value="Enviar"/><br>
+        <input type="submit" value="Enviar"/><br>
+        <form class="form-horizontal">
+            <div class="form-group">
+                <label for="note3" class="col-sm-2 control-label">Nome</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="note3" placeholder="Nome">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="inputEmail3" class="col-sm-2 control-label">E-Mail</label>
+                <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="example@example.com">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+                <div class="col-sm-10">
+                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                    <div class="radio">
+                        <label>
+                            Tipo de Usuário:<br>
+                            <input type="radio" name="Tipo" Value="1"> Administrador
+                            <input type="radio" name="Tipo" Value="2"> Autor
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                    <button type="submit" class="btn btn-default">Cadastrar</button>
+                </div>
+            </div>
         </form>
-        
     </body>
 </html>
